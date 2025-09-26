4786603d40303dc3a590160aba903fbd058c31d9
kurumii86 <kurumithokisaki08@gmail.com>
2025-09-26 17:25:15 +0800

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -282,5 +282,4 @@ body {
         </div>
     </div>
 </div>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> 
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"> 
